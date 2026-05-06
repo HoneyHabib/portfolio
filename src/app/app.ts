f6aa469930f030220914cd4d69f9a4139af8c546
HoneyHabib <66374715+HoneyHabib@users.noreply.github.com>
2026-05-06 17:03:20 +0530

    Add UI components and assets; update deps
    Add core UI components (navbar, footer, floating-social) and feature components (Hero, about, experience, projects, skill) with templates, styles and unit tests. Add assets (PDF, fonts, hero image, favicon) and update app files (app.config, routes, app.ts, app.html, index.html, styles). Update angular.json assets config to use src assets, remove public/favicon.ico, and update package.json/package-lock to include FontAwesome and gh-pages publishing tooling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Footer} from './core/footer/footer';
+import { Navbar } from './core/navbar/navbar';
+import {Hero} from './features/Hero/Hero';
+import { Projects } from './features/projects/projects';
+import { Experience } from './features/experience/experience';
+import { About } from './features/about/about';
+import { FloatingSocial } from './core/floating-social/floating-social';
+import { Skill } from './features/skill/skill';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    Navbar,
+    Hero,
+    FloatingSocial,
+    About,
+    Experience,
+    Skill,
+    Footer,
+    Projects
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
